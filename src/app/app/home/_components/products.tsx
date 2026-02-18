860b9dc59eb3b497e727627319f3243bce519c2b
steveparker123 <176498134+steveparker123@users.noreply.github.com>
2026-02-18 19:59:26 +0500

    create: notification ui and integration

--- src/app/app/home/_components/products.tsx
+++ src/app/app/home/_components/products.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import SwiperProducts from "./swiper-products";
 import { useProducts } from "@/src/lib/api/products";
 import { useSearchParams } from "next/navigation";
